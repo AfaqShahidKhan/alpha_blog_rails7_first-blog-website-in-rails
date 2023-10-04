734c811d6df9adbd1d8da761637cd8b1e312f55b
Afaq Khan <afaqk3203@gmail.com>
2023-10-04 20:36:11 +0500

    migration

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
 class Article < ApplicationRecord
+validates :title, presence: true, length: {minimum:6, maximum:116}
+validates :description, presence: true, length: {minimum:6, maximum:116}
+
 
 end
