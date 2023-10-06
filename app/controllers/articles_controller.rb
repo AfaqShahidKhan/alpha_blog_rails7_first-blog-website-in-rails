7a4c1c368d7448ec9c4c540ff06add80eab5783a
Afaq Khan <afaqk3203@gmail.com>
2023-10-06 19:42:33 +0500

    add show page

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,11 @@
+class ArticlesController < ApplicationController
+
+  def show
+
+  end
+  def index
+
+  end
+
+
+  end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_03_190949) do
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 0) do
 end
