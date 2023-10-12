c442ca1895ae8c260eabcce387b12457fd77349d
Afaq Khan <afaqk3203@gmail.com>
2023-10-12 19:39:55 +0500

    update deployement

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,5 @@
 class ArticlesController < ApplicationController
-
-before_action :set_article, only: [:edit, :update, :show, :destory]
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def show
 
@@ -42,12 +41,10 @@ before_action :set_article, only: [:edit, :update, :show, :destory]
 
     end
   end
-   def destroy
-
-      @article.destroy
-      redirect_to articles_path
-
-   end
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
 
   private
 
