ebbafe38c1f45d69a4848b34b8d36152b92db693
Afaq Khan <afaqk3203@gmail.com>
2023-10-17 00:00:50 +0500

    show user on pages

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, length: { minimum: 6, maximum: 255 }
   validates :description, presence: true, length: { minimum: 6, maximum: 255 }
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_16_145019) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_16_182838) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
