96206b21832052f3f9aaa81cfa7b1a717dfff1dd
Afaq Khan <afaqk3203@gmail.com>
2023-10-17 23:29:05 +0500

    create show page for user details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,15 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  def show
+  @user = User.find(params[:id])
+  @articles = @user.articles
+  end
 
   def create
     @user = User.new(user_params)
     if @user.save
-    flash[:notice]="Welcome to Alpha blog, you are signed up"
+    flash[:notice]="Welcome #{@user.username} to Alpha blog, you are signed up"
     redirect_to articles_path
     else
       render 'new'
