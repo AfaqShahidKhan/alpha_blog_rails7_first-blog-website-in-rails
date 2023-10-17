5ffd69693ea4b56dde7f23012ea07566b6cd0340
Afaq Khan <afaqk3203@gmail.com>
2023-10-17 22:31:43 +0500

    add edit and update user functionalities

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    flash[:notice]="Welcome to Alpha blog, you are signed up"
+    redirect_to articles_path
+    else
+      render 'new'
+    end
+
+  end
+
+  def edit
+    @user = User.find(params[:id])
+
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice]="User successfully updated!"
+      redirect_to articles_path
+      else
+        render 'edit'
+    end
+
+  end
+
+
+  private
+  def user_params
+  params.require(:user).permit(:username, :email, :password)
+  end
+
+
+end
