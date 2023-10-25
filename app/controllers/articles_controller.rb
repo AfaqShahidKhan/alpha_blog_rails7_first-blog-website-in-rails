ef3582c4752897b673b74ce839a866356af1e527
Afaq Khan <afaqk3203@gmail.com>
2023-10-25 22:06:46 +0500

    add auth methods to controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,7 +22,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
      if @article.save
        flash[:notice]="Article is created successfully!"
        redirect_to @article
