fb00dd1e1693fb57e706ad3042a1e38a1d0ce0fc
Afaq Khan <afaqk3203@gmail.com>
2023-10-28 08:54:40 +0500

    change the category migration

--- db/migrate/20231027194202_create_category.rb
+++
@@ -1,8 +0,0 @@
-class CreateCategory < ActiveRecord::Migration[7.0]
-  def change
-    create_table :categories do |t|
-      t.string name: 'custom_name'
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_25_194740) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_28_034722) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
