6c6d33c5e5405797c3e4a0b8ad4955cda297aaf0
Afaq Khan <afaqk3203@gmail.com>
2023-10-30 00:30:10 +0500

    integration tests tricky

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,17 +1,40 @@
 class CategoriesController < ApplicationController
-
+  before_action :require_admin, except: [:index, :show]
 
   def index
-
+    @categories = Category.paginate(page: params[:page], per_page: 5)
   end
 
   def new
 
+    @category = Category.new
   end
 
   def show
+    @category = Category.find(params[:id])
+  end
 
+  def create
+  @category = Category.new(category_params)
+  if @category.save
+  flash[:notice]="Category created successfully"
+    redirect_to @category
+  else
+    render 'new'
   end
 
 
+
+  end
+
+  private
+  def category_params
+   params.require(:category).permit(:name)
+  end
+  def require_admin
+    if !(logged_in? && current_user.admin?)
+      flash[:alert] = "Only admins can perform that action"
+      redirect_to categories_path
+    end
+  end
 end
